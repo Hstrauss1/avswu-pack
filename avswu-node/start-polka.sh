bd5a7f23b165407db39972a49c00bf6fdd36a55f
Hudson Strauss <122407968+Hstrauss1@users.noreply.github.com>
2025-03-09 15:58:04 -0700

    Add files via upload

--- avswu-node/start-polka.sh
+++ avswu-node/start-polka.sh
@@ -11,5 +11,5 @@ echo "STATUS:       Starting substrate contract node..."
 echo "------------------------------------------------------------------"
 
 # web socket port defaults to 9944
-cd /usr/local/substrate/substrate-contracts-node-linux
+cd /usr/substrate-contracts-node-linux
 ./substrate-contracts-node --log info,runtime::contracts=debug 2>&1 --rpc-cors all --unsafe-rpc-external --name test-node --dev
